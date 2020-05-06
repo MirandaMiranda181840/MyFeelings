c856ae6843398b03c460dc725c0046103c7c55e0
Miranda Miranda <miranda.miranda@potros.itson.edu.mx>
2020-05-06 00:12:11 -0700

    Primer commit

---
+++ gradlew.bat
@@ -0,0 +1,45 @@
+{
+    "logs": [
+        {
+            "outputFile": "C:\\Users\\miran\\AndroidStudioProjects\\ImHungry-Android\\app\\build\\intermediates\\incremental\\mergeDebugResources\\merged.dir\\values-hu\\values-hu.xml",
+            "map": [
+                {
+                    "source": "C:\\Users\\miran\\.gradle\\caches\\transforms-2\\files-2.1\\b6051f73dfa72a739966d5778dbdeec2\\core-1.0.2\\res\\values-hu\\values-hu.xml",
+                    "from": {
+                        "startLines": "2",
+                        "startColumns": "4",
+                        "startOffsets": "55",
+                        "endColumns": "100",
+                        "endOffsets": "151"
+                    },
+                    "to": {
+                        "startLines": "50",
+                        "startColumns": "4",
+                        "startOffsets": "6110",
+                        "endColumns": "100",
+                        "endOffsets": "6206"
+                    }
+                },
+                {
+                    "source": "C:\\Users\\miran\\.gradle\\caches\\transforms-2\\files-2.1\\6ee598e9deb5d49f5846ae98356625e7\\jetified-play-services-basement-17.1.1\\res\\values-hu\\values.xml",
+                    "from": {
+                        "startLines": "4",
+                        "startColumns": "0",
+                        "startOffsets": "247",
+                        "endColumns": "235",
+                        "endOffsets": "482"
+                    },
+                    "to": {
+                        "startLines": "37",
+                        "startColumns": "4",
+                        "startOffsets": "4116",
+                        "endColumns": "235",
+                        "endOffsets": "4347"
+                    }
+                },
+                {
+                    "source": "C:\\Users\\miran\\.gradle\\caches\\transforms-2\\files-2.1\\71a63f06f2596ec1dfbba5264bc337ce\\jetified-play-services-base-17.1.0\\res\\values-hu\\values.xml",
+                    "from": {
+                        "startLines": "4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20",
+                        "startColumns": "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0",
+                        "startOffsets": "245,354,570,706,813,1040,1176,1324,1459,1
